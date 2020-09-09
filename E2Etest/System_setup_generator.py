15a5e5681e7953392af17f8e545336758b74d21d
zhz03 <zhz03@g.ucla.edu>
2020-09-09 14:28:35 -0500

    added some new codes

--- E2Etest/System_setup_generator.py
+++ E2Etest/System_setup_generator.py
@@ -6,10 +6,13 @@ Created on Wed Sep  9 14:09:33 2020
 """
 
 def System_setup_generator():
-    T = 100
+    T = 101
     uts = [0,1]
     ts = [100,1]
     ut = [0,1]
     trials = 1000
+
     return T,uts,ts,ut,trials
 
+if __name__ == '__main__':
+    T,uts,ts,ut,trials = System_setup_generator()
