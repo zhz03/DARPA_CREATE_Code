f078560006981995f3d292d5c6bc7970314a9e44
zhz03 <zhz03@g.ucla.edu>
2020-09-09 14:18:10 -0500

    changed the name of the file

---
+++ E2Etest/System_setup_generator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 14:09:33 2020
+
+@author: Zhaoliang
+"""
+
+def System_setup_generator():
+    T = 100
+    uts = [0,1]
+    ts = [100,1]
+    ut = [0,1]
+    trials = 1000
+    return T,uts,ts,ut,trials
+
