24dac21bd2e68b47a744b0c6c9cc9b5531e15c1a
Zhaoliang <zhz03@g.ucla.edu>
2020-09-10 11:30:40 -0500

    Changed inputs of other functions

--- E2Etest/E2E_validation.py
+++ E2Etest/E2E_validation.py
@@ -10,15 +10,10 @@ import Simulation.Binary_stat_hypothesis_testing_1d as BstatHT1d
 import Simulation.Simulation as Sim
 import E2Etest.Error_prob_comparison as EPComp
 
-def E2E_validation(SM,T,ut,uts,ts,trials):
-    A = SM[0]
-    B = SM[1]
-    H = SM[2]
-    Q = SM[3]
-    R = SM[4]
-    Pr_D,Pr_FA,Pr_M,Pr_CR = Spr.Sensor_planner_1d(A,B,H,Q,R,T,ut)
-    x0 =  np.array([[0]]).reshape(1, 1)
-    u_T_D = Sim.simulation(A,B,H,Q,R,x0,uts,ts,ut,trials)
+def E2E_validation(SM,T,ut,x0,uts,ts,trials):
+
+    Pr_D,Pr_FA,Pr_M,Pr_CR = Spr.Sensor_planner_1d(SM,T,ut)
+    u_T_D = Sim.simulation(SM,x0,uts,ts,ut,trials)
     Pr_D_stat,Pr_FA_stat,Pr_M_stat,Pr_CR_stat = BstatHT1d.Bin_stat_hyp_test_1d(u_T_D)
     error_D,error_FA,error_M,error_CR = EPComp.Error_prob_Comp(Pr_D,Pr_FA,Pr_M,Pr_CR,Pr_D_stat,Pr_FA_stat,Pr_M_stat,Pr_CR_stat)
     return error_D,error_FA,error_M,error_CR
@@ -45,7 +40,7 @@ if __name__ == '__main__':
     trials = 1000
     T = 101
     
-    error_D,error_FA,error_M,error_CR = E2E_validation(SM,T,ut,uts,ts,trials)
+    error_D,error_FA,error_M,error_CR = E2E_validation(SM,T,ut,x0,uts,ts,trials)
 
     #Pr_D,Pr_FA,Pr_M,Pr_CR = Spr.Sensor_planner_1d(A,B,H,Q,R,T,ut)
     #u_T_D = Sim.simulation(A,B,H,Q,R,x0,uts,ts,ut,trials)
