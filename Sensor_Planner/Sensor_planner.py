bac3d2e0d69215ed8f150375553547e526fc48b8
Zhaoliang <zhz03@g.ucla.edu>
2020-09-09 19:44:09 -0500

    Changed sensor planner inputs to match the flowchart

--- Sensor_Planner/Sensor_planner.py
+++ Sensor_Planner/Sensor_planner.py
@@ -11,12 +11,13 @@ import Sensor_Planner.KF_planner_nd as kfplnnd
 import Sensor_Planner.Ut_planner_nd as Uplnnd
 import Sensor_Planner.Binary_hypothesis_testing_1d as Bht1d
 
-def Sensor_planner_1d(A,B,H,Q,R,T,ut):
+def Sensor_planner_1d(SM,T,ut):
     Sigma_xhat = kfplnnd.KF_planner(A, B, H, Q, R,T)
     Sigma_ut = Uplnnd.Ut_planner_nd(A, B, H, Q, R,Sigma_xhat)
     Sigma_uts = [Sigma_ut,Sigma_ut]
     Prob_D,Prob_FA,Prob_M,Prob_CR = Bht1d.Bin_Hyp_test_1d(Sigma_uts,ut)
     return Prob_D,Prob_FA,Prob_M,Prob_CR    
+
 if __name__ == '__main__':
     # system model
     dx = 1
@@ -30,6 +31,7 @@ if __name__ == '__main__':
     T = 100
     # ut
     ut = [0,1]
-    Prob_D,Prob_FA,Prob_M,Prob_CR = Sensor_planner_1d(A,B,H,Q,R,T,ut)
+    SM = [A,B,H,Q,R]
+    Prob_D,Prob_FA,Prob_M,Prob_CR = Sensor_planner_1d(SM,T,ut)
 
     
