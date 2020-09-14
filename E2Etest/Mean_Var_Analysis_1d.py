5c275f21bd1e355ebfc862b444847d1994dd774c
Zhaoliang <zhz03@g.ucla.edu>
2020-09-14 15:47:06 -0500

    finished mean and variance analysis

--- E2Etest/Mean_Var_Analysis_1d.py
+++ E2Etest/Mean_Var_Analysis_1d.py
@@ -5,5 +5,13 @@ Created on Tue Sep  8 15:34:45 2020
 @author: Zhaoliang
 """
 
-def M_Cov_Analy():
+import numpy as np
+
+def M_Cov_Analy(data):
+    mean = np.mean(data)
+    var = np.var(data) 
+    return mean,var
     
+if __name__ =="__main__":
+    x = [np.random.normal(0,1) for i in range(1000)]
+    x_mean,x_var = M_Cov_Analy(x)
