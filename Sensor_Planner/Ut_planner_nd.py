630a8aa26e56294b56bcf667203ba8d24a2c36e4
Zhaoliang <zhz03@g.ucla.edu>
2020-09-14 15:58:52 -0500

    run 1d test to debug

--- Sensor_Planner/Ut_planner_nd.py
+++ Sensor_Planner/Ut_planner_nd.py
@@ -18,12 +18,12 @@ def Ut_planner_nd(SM,Sigma_xhat):
     U_sigma = np.dot(np.dot(H,S),H.T)+R
     return U_sigma
 
-if __name__ == '__main__':   
+if __name__ == '__main__':
+    """
     dx = 2
     dz = 2
     q = np.array([[0.2,0.4],[0.4,0.2]]).reshape(dx, dx)
     r = np.array([[0.2,0],[0,0.2]]).reshape(dz, dz)
-    
     A = np.array([[1,0],[0,1]]).reshape(dx, dx)
     B = np.array([[1],[1]]).reshape(dx, 1) 
     H = np.array([[1,0],[0,1]]).reshape(dz, dx)
@@ -31,7 +31,21 @@ if __name__ == '__main__':
     R = np.dot(r,r.T)
     SM = [A,B,H,Q,R]
     x0 = np.array([[0],[0]]).reshape(dx, 1)
-
-    T = 100  
+    """
+    dx = 1
+    dz = 1
+    # system matrices parameters
+    q = 2.0
+    r = 1.0
+    a = 1
+    h = 1
+    b = 1
+    A = np.array([a]).reshape(dx, dx)
+    H = np.array([h]).reshape(dz, dx)
+    B = np.array([b]).reshape(dx, 1)
+    Q = np.array([q * q]).reshape(dx, dx)
+    R = np.array([r * r]).reshape(dz, dz)
+    SM = [A,B,H,Q,R] 
+    T = 0  
     Sigma_xhat = kfplnnd.KF_planner(SM,T)
     Sigma_ut= Ut_planner_nd(SM,Sigma_xhat)
