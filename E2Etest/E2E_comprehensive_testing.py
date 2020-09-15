dc2d3106024249835abe7306ab60392608e96a28
Zhaoliang <zhz03@g.ucla.edu>
2020-09-15 02:55:38 -0500

    added time calculation and save the errors to file for other process

--- E2Etest/E2E_comprehensive_testing.py
+++ E2Etest/E2E_comprehensive_testing.py
@@ -4,23 +4,29 @@ Created on Tue Sep  8 15:23:58 2020
 
 @author: Zhaoliang
 """
-
+import numpy as np
 import E2Etest.E2E_validation as E2Eval
 import E2Etest.SM_generator_1d as SMGen1d
 import E2Etest.System_setup_generator as SSGen
+import datetime
 
 def E2E_com_test():
     System_models = SMGen1d.SM_generator_1d()
     #As,Hs,Bs,Qs,Rs,
     T,uts,ts,ut,trials,x0 = SSGen.System_setup_generator()
-    SM_num = len(System_models)
+    SM_num = len(System_models[0])
     
     Error_D = []
     Error_FA = []
     Error_M = []
     Error_CR = []
-    for i in range(SM_num):
-        SM = System_models[i]
+    for i in range(3):
+        A = System_models[0][i]
+        B = System_models[1][i]
+        H = System_models[2][i]
+        Q = System_models[3][i]
+        R = System_models[4][i]
+        SM = [A,B,H,Q,R]
         """
         A = As[i]
         B = Bs[i]
@@ -28,7 +34,8 @@ def E2E_com_test():
         Q = Qs[i]
         R = Rs[i]
         """
-        error_D,error_FA,error_M,error_CR = E2Eval.E2E_validation(SM,T,ut,uts,ts,trials,x0)
+        error_D,error_FA,error_M,error_CR = E2Eval.E2E_validation(SM,T,ut,x0,uts,ts,trials)
+        
         Error_D.append(error_D)
         Error_FA.append(error_FA)
         Error_M.append(error_M)
@@ -37,5 +44,12 @@ def E2E_com_test():
     return Error_D,Error_FA,Error_M,Error_CR
 
 if __name__ == "__main__":
+    starttime = datetime.datetime.now()
 
     Error_D,Error_FA,Error_M,Error_CR = E2E_com_test()
+    np.save('Error_D.npy', Error_D)
+    np.save('Error_FA.npy', Error_FA)
+    np.save('Error_M.npy', Error_M)
+    np.save('Error_CR.npy', Error_CR)
+    endtime = datetime.datetime.now()
+    print ((endtime - starttime).seconds)
