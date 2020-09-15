d4ca72aa19a5aa2d18371187faca2df783b5c53f
Zhaoliang <zhz03@g.ucla.edu>
2020-09-15 02:29:47 -0500

    verify estimator in the E2E_validation

--- E2Etest/E2E_validation.py
+++ E2Etest/E2E_validation.py
@@ -42,6 +42,20 @@ if __name__ == '__main__':
     ut = [0,1] 
     trials = 1000
     T = 101
+    """    
+    dx = 1
+    dz = 1
+    q = .3
+    r = .2
+    a = 1
+    h = .1
+    b = 1
+    A = np.array([a]).reshape(dx, dx)
+    H = np.array([h]).reshape(dz, dx)
+    B = np.array([b]).reshape(dx, 1)
+    Q = np.array([q * q]).reshape(dx, dx)
+    R = np.array([r * r]).reshape(dz, dz)
+    SM = [A,B,H,Q,R]
     """
     System_models = SMGen1d.SM_generator_1d()
 
@@ -51,12 +65,14 @@ if __name__ == '__main__':
     Q = System_models[3][0]
     R = System_models[4][0]
     SM = [A,B,H,Q,R]
+    """
     T,uts,ts,ut,trials,x0 = SSGen.System_setup_generator()
+
     #error_D,error_FA,error_M,error_CR = E2E_validation(SM,T,ut,x0,uts,ts,trials)
     
     Pr_D,Pr_FA,Pr_M,Pr_CR = Spr.Sensor_planner_1d(SM,T,ut)
+    
     u_T_D = Sim.simulation(SM,x0,uts,ts,ut,trials)
-    """
     Pr_D_stat,Pr_FA_stat,Pr_M_stat,Pr_CR_stat = BstatHT1d.Bin_stat_hyp_test_1d(u_T_D)
     error_D,error_FA,error_M,error_CR = EPComp.Error_prob_Comp(Pr_D,Pr_FA,Pr_M,Pr_CR,Pr_D_stat,Pr_FA_stat,Pr_M_stat,Pr_CR_stat)
-    """
+    
