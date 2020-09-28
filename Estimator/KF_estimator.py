637677875fc6a40de1d76818879031699ecdf4a2
Zhaoliang <zhz03@g.ucla.edu>
2020-09-28 11:49:55 -0500

    debug estimator

--- Estimator/KF_estimator.py
+++ Estimator/KF_estimator.py
@@ -10,7 +10,8 @@ import Simulation.Generate_seq_u as Gsequ
 import Simulation.Simulator as Simu
 import utility_functions.plot_figures as plotfgs
 import utility_functions.convert_data as cnvdata
-
+import Estimator.Bayesian_analysis as BA
+import Estimator.Decision_making as DM
 
 class KalmanFilter(object):
     def __init__(self, A = None, B = None, H = None, Q = None, R = None, P = None, x0 = None):
@@ -75,6 +76,7 @@ if __name__ == '__main__':
     ts = [100,10,20,10]
     SM = [A,B,H,Q,R]
     """
+    """
     dx = 1
     dz = 1
     # system matrices parameters
@@ -94,11 +96,38 @@ if __name__ == '__main__':
     ut = [0,1] 
     trials = 1000
     SM = [A,B,H,Q,R]
+    """
+    dx = 1
+    dz = 1
+    q = .1254
+    r = 1.6798
+    r = .1254
+    a = 1.4816
+    #a = 1.0
+    h = 1.5927
+    b = 1
+    A = np.array([a]).reshape(dx, dx)
+    H = np.array([h]).reshape(dz, dx)
+    B = np.array([b]).reshape(dx, 1)
+    Q = np.array([q]).reshape(dx, dx)
+    R = np.array([r]).reshape(dz, dz)
+    x0 = np.array([[0]]).reshape(dx, 1)
+    
+    uts = [0,1]
+    ts = [100,10]
+    ut = [0,1] 
+    trials = 1000
+    SM = [A,B,H,Q,R]
     
     u = Gsequ.generate_sequential_ut(uts,ts)
     y,z = Simu.Simulator(SM,x0,u)
     Sigma,estimates = KF_estimator(SM,z)
-    ut_zt = z[-1] - np.dot(H,np.dot(A,estimates[-2]))
+    S = np.dot(H,np.dot(A,estimates[-2]))
+    ut_zt1 = z[-1] - S
+    
+    ut_zt,Sigma_ut_zt = BA.Bayesian_analysis(SM,Sigma,estimates,z)
+    ut = [np.dot(H,B)*ut[0],np.dot(H,B)*ut[1]]
+    u_D = DM.Decision_making(ut,ut_zt,Sigma_ut_zt)
     #ut_zt1 = z[-1] - np.dot(H,predictions[-2])
     # Plot figure
     
