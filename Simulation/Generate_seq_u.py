2cb574ec141c013884f9f0b53fcacd14f2f13a4e
Zhaoliang <zhz03@g.ucla.edu>
2020-09-28 16:16:29 -0500

    test save multiple figures

--- Simulation/Generate_seq_u.py
+++ Simulation/Generate_seq_u.py
@@ -5,6 +5,7 @@ Created on Mon Sep  7 22:50:59 2020
 @author: Zhaoliang
 """
 import utility_functions.plot_figures as plotfgs
+import matplotlib.pyplot as plt
 
 def generate_sequential_ut(uts,ts):
     un = len(uts)
@@ -16,10 +17,16 @@ def generate_sequential_ut(uts,ts):
     return ut_sq
 
 #def verification(uts,ts):    
-
+   
 if __name__ == '__main__':
     uts = [0,1,0]
     ts = [100,10,20]
     ut_sq = generate_sequential_ut(uts,ts)
     plotfgs.plot_generate_seq_u(ut_sq,uts,ts)
+    plt.savefig('./figs/Gen_seq_1.jpg') 
     
+    uts = [0,1,2]
+    ts = [100,10,20]
+    ut_sq = generate_sequential_ut(uts,ts)
+    plotfgs.plot_generate_seq_u(ut_sq,uts,ts)
+    plt.savefig('./figs/Gen_seq_2.jpg') 
