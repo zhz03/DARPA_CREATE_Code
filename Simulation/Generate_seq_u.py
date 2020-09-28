7a0271649398ff6796dbc239e90dbfa03901c82a
Zhaoliang <zhz03@g.ucla.edu>
2020-09-28 16:26:50 -0500

    single random test

--- Simulation/Generate_seq_u.py
+++ Simulation/Generate_seq_u.py
@@ -6,6 +6,7 @@ Created on Mon Sep  7 22:50:59 2020
 """
 import utility_functions.plot_figures as plotfgs
 import matplotlib.pyplot as plt
+import random
 
 def generate_sequential_ut(uts,ts):
     un = len(uts)
@@ -16,17 +17,29 @@ def generate_sequential_ut(uts,ts):
             ut_sq.append(uts[i])    
     return ut_sq
 
-#def verification(uts,ts):    
-   
-if __name__ == '__main__':
-    uts = [0,1,0]
-    ts = [100,10,20]
+def fig_verification(uts,ts,i):    
     ut_sq = generate_sequential_ut(uts,ts)
     plotfgs.plot_generate_seq_u(ut_sq,uts,ts)
-    plt.savefig('./figs/Gen_seq_1.jpg') 
+    fig_name = './figs/Gen_seq_figs/' + str(i) + '.jpg'
+    plt.savefig(fig_name)     
+
+def single_randome_test():
+    #initialize uts and ts 
+    uts = [0,0,0]
+    ts = [10,10,10]
+    for i in range(3):
+        uts[i] = round(random.uniform(0,4))
+        ts[i] = round(random.uniform(10,100))
+    fig_verification(uts,ts,i)  
+
+
+if __name__ == '__main__':
+    single_randome_test()
     
+    """
     uts = [0,1,2]
     ts = [100,10,20]
     ut_sq = generate_sequential_ut(uts,ts)
     plotfgs.plot_generate_seq_u(ut_sq,uts,ts)
     plt.savefig('./figs/Gen_seq_2.jpg') 
+    """
