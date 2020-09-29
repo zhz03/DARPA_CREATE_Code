3687d72ebf74a5f5bb261cfb2b5415877327c468
Zhaoliang <zhz03@g.ucla.edu>
2020-09-29 00:11:42 -0500

    test code

--- Estimator/estimator.py
+++ Estimator/estimator.py
@@ -13,20 +13,62 @@ import Simulation.Generate_seq_u as Gsequ
 import Simulation.Simulator as Simu
 import utility_functions.plot_figures as plotfgs
 import utility_functions.convert_data as cnvdata
-
+import E2Etest.SM_generator_1d as SMGen1d
+import E2Etest.System_setup_generator as SSGen
+import matplotlib.pyplot as plt
+import utility_functions.CompP2SHist as CompP2SHist
 
 def estimator(SM,z,ut):
     Sigma,estimates = KF_est.KF_estimator(SM,z)
-    ut_zt,Sigma_ut_zt = BA.Bayesian_analysis(SM,Sigma,estimates,z)
-    H = SM[2]
-    B = SM[1]
-    ut = [np.dot(H,B)*ut[0],np.dot(H,B)*ut[1]]
-    u_D = DM.Decision_making(ut,ut_zt,Sigma_ut_zt)
+    ut_zt,mean_ut_zt,Sigma_ut_zt = BA.Bayesian_analysis(SM,ut,Sigma,estimates,z)
+    u_D = DM.Decision_making(ut,ut_zt,mean_ut_zt,Sigma_ut_zt)
     return u_D
 
 if __name__ == '__main__':
+    num = 1
     dx = 1
-    dz = 1
+    Arange = [1,1]
+    Brange = [1,1]
+    Hrange = [0,2]
+    Qrange = [0,2]    
+    Rrange = [0,2]
+    System_models = SMGen1d.SM_generator_1d(num,Arange,Brange,Hrange,Qrange,Rrange)
+    #As,Hs,Bs,Qs,Rs,
+    T,uts,ts,ut,trials,x0 = SSGen.System_setup_generator()
+    SM_num = len(System_models[0])
+    T = 1000
+    ts[0] = T
+    for i in range(SM_num):
+        A = System_models[0][i]
+        B = System_models[1][i]
+        H = System_models[2][i]
+        Q = System_models[3][i]
+        R = System_models[4][i]
+        SM = [A,B,H,Q,R]
+        u = Gsequ.generate_sequential_ut(uts,ts)
+        y,z = Simu.Simulator(SM,x0,u)
+        #==============estimator===============================
+        Sigma,estimates = KF_est.KF_estimator(SM,z)
+        ut_zt,mean_ut_zt,Sigma_ut_zt = BA.Bayesian_analysis(SM,ut,Sigma,estimates,z)
+        u_D = DM.Decision_making(ut,ut_zt,mean_ut_zt,Sigma_ut_zt)
+        #======================kf_estimator_verification===============================
+        ground_truth = cnvdata.convert_array2list_nd(y,dx)
+        measurements = cnvdata.convert_array2list_nd(z,dx) 
+        estimates = cnvdata.convert_array2list_nd(estimates,dx)
+        errors = [estimates[0][i] - ground_truth[0][i] for i in range(len(estimates[0]))]
+        plotfgs.multiKf_plot(measurements,ground_truth,estimates,kfest_flag = True)
+        plt.title('A=' + '%.2f' % A + '; B=' + '%.2f' % B + '; H=' + '%.2f' % H + '; Q=' + '%.2f' % Q + '; R=' + '%.2f' % R)
+        fig_name = './figs/estimator_figs/' + str(i) + 'kf_estimator.jpg'
+        plt.savefig(fig_name)
+        plt.close()
+
+        CompP2S = CompP2SHist.Compare_pln2statis_hist(mean_pln = 0, Sigma_pln = Sigma[-1][0][0])
+        CompP2S.visualization_compare(errors,1)
+        plt.title('A=' + '%.2f' % A + '; B=' + '%.2f' % B + '; H=' + '%.2f' % H + '; Q=' + '%.2f' % Q + '; R=' + '%.2f' % R)
+        fig_name1 = './figs/estimator_figs/' + str(i) + '_error.jpg'
+        plt.savefig(fig_name1)
+        plt.close()
+
     # system matrices parameters
     """
     q = 0.3
@@ -44,6 +86,7 @@ if __name__ == '__main__':
     ts = [100,1]
     ut = [0,1]
     SM = [A,B,H,Q,R]
+    """
     """
     dx = 1
     dz = 1
@@ -76,6 +119,6 @@ if __name__ == '__main__':
     Sigma = cnvdata.convert_array2list_nd(Sigma,dx)
     
     plotfgs.multiKf_plot(measurements,ground_truth,estimates,kfest_flag = True)
-
+    """
     #u_D = estimator(SM,z,ut)
     
