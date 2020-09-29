45cec43544b32b6ea9a38327d14292fbdf3274d8
Zhaoliang <zhz03@g.ucla.edu>
2020-09-28 21:47:40 -0500

    test code in the main functoin

--- Estimator/Bayesian_analysis.py
+++ Estimator/Bayesian_analysis.py
@@ -12,6 +12,8 @@ import Simulation.Simulator as Simu
 import utility_functions.convert_data as cnvdata
 import utility_functions.CompP2SHist as CompP2SHist
 import matplotlib.pyplot as plt
+import E2Etest.SM_generator_1d as SMGen1d
+import E2Etest.System_setup_generator as SSGen
 
 def Bayesian_analysis(SM,Sigma,estimates,measurements):
     A = SM[0]
@@ -25,37 +27,43 @@ def Bayesian_analysis(SM,Sigma,estimates,measurements):
     Sigma_ut_zt = np.dot(np.dot(H,S),H.T)+R
     return ut_zt,Sigma_ut_zt
 
+#def verification():
+    
 if __name__ == '__main__':
+    num = 10
     dx = 1
-    dz = 1
-    # system matrices parameters
-    q = 0.2
-    r = 0.1
-    a = 1
-    h = 1
-    b = 1
-    A = np.array([a]).reshape(dx, dx)
-    H = np.array([h]).reshape(dz, dx)
-    B = np.array([b]).reshape(dx, 1)
-    Q = np.array([q * q]).reshape(dx, dx)
-    R = np.array([r * r]).reshape(dz, dz)
-    x0 = np.array([[0]]).reshape(dx, 1)
-    uts = [0,1]
-    ts = [100,10]
-    samp_num = 1000
-    Ut_zt = []
-    for i in range(samp_num):
-        u = Gsequ.generate_sequential_ut(uts,ts)
+    Arange = [1,1]
+    Brange = [1,1]
+    Hrange = [0,2]
+    Qrange = [0,2]    
+    Rrange = [0,2]
+    System_models = SMGen1d.SM_generator_1d(num,Arange,Brange,Hrange,Qrange,Rrange)
+    #As,Hs,Bs,Qs,Rs,
+    T,uts,ts,ut,trials,x0 = SSGen.System_setup_generator()
+    SM_num = len(System_models[0])
+    for i in range(num):
+        A = System_models[0][i]
+        B = System_models[1][i]
+        H = System_models[2][i]
+        Q = System_models[3][i]
+        R = System_models[4][i]
         SM = [A,B,H,Q,R]
-        y,z = Simu.Simulator(SM,x0,u)
-        Sigma,estimates = KF_est.KF_estimator(SM,z)
-        ut_zt,Sigma_ut_zt = Bayesian_analysis(SM,Sigma,estimates,z)
-        Ut_zt.append(ut_zt)
-    Ut_zt = cnvdata.convert_array2list_nd(Ut_zt,dx)
-    mean_pln = np.dot(H,B)*uts[1]
-    CompP2S = CompP2SHist.Compare_pln2statis_hist(mean_pln = np.mean(Ut_zt), Sigma_pln = Sigma_ut_zt[0][0])
-    CompP2S.visualization_compare(Ut_zt[0],10)
-    plt.title('A=' + '%.2f' % A + '; B=' + '%.2f' % B + '; H=' + '%.2f' % H + '; Q=' + '%.2f' % Q + '; R=' + '%.2f' % R)
+        Ut_zt = []
+        samp_num = 100
+        for j in range(samp_num):
+            u = Gsequ.generate_sequential_ut(uts,ts)
+            y,z = Simu.Simulator(SM,x0,u)
+            Sigma,estimates = KF_est.KF_estimator(SM,z)
+            ut_zt,Sigma_ut_zt = Bayesian_analysis(SM,Sigma,estimates,z)
+            Ut_zt.append(ut_zt)
+        Ut_zt = cnvdata.convert_array2list_nd(Ut_zt,dx)
+        mean_pln = np.dot(H,B)*uts[1]
+        CompP2S = CompP2SHist.Compare_pln2statis_hist(mean_pln = np.mean(Ut_zt), Sigma_pln = Sigma_ut_zt[0][0])
+        CompP2S.visualization_compare(Ut_zt[0],1)
+        plt.title('A=' + '%.2f' % A + '; B=' + '%.2f' % B + '; H=' + '%.2f' % H + '; Q=' + '%.2f' % Q + '; R=' + '%.2f' % R)
+        fig_name = './figs/Bayesian_analysis_figs/' + str(i) + '_UtztHist.jpg'
+        plt.savefig(fig_name)
+        plt.close()
     
     """
     ground_truth = cnvdata.convert_array2list_nd(y,dx)
