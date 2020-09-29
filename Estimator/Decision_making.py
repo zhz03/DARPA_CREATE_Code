3b535a3fed43a61fa531ae59d2770362f123e606
Zhaoliang <zhz03@g.ucla.edu>
2020-09-28 22:48:14 -0500

    test code

--- Estimator/Decision_making.py
+++ Estimator/Decision_making.py
@@ -11,21 +11,44 @@ import Estimator.Bayesian_analysis as BA
 import Simulation.Generate_seq_u as Gsequ
 import Simulation.Simulator as Simu
 import utility_functions.convert_data as cnvdata
-
+import utility_functions.plot_figures as plotfgs
+import binary_HT_1d.Gaussian_dist as Gd
+import random
+import matplotlib.pyplot as plt
 
 def Decision_making(ut,ut_zt,Sigma_ut_zt):
     ut0 = ut[0]
     ut1 = ut[1]
     pdf_H0 = multivariate_normal(mean=ut0,cov=Sigma_ut_zt).pdf(ut_zt) 
     pdf_H1 = multivariate_normal(mean=ut1,cov=Sigma_ut_zt).pdf(ut_zt) 
-    if pdf_H0 > pdf_H1:
-        decide_u = 0
-    else: decide_u = 1
+    if pdf_H0 >= pdf_H1:
+        decide_u = ut0
+    else: decide_u = ut1
     return decide_u
 
 if __name__ == '__main__':
-
-    ut_zt = 0.8
-    Sigma_ut_zt = 0.5    
-    ut = [0,1]
+    # randomly create some ut
+    range1 = -3
+    range2 = 3
+    a = round(random.uniform(range1,range2));
+    b = round(random.uniform(range1,range2));
+    while a == b:
+        a = round(random.uniform(range1,range2));
+    if a > b:
+        temp = a
+        a = b 
+        b = temp        
+    ut = [a,b]
+    Sigma_ut_zt = random.uniform(0,range2)
+    ut_zt = random.uniform(range1,range2)
     u_D = Decision_making(ut,ut_zt,Sigma_ut_zt)
+    # plot figure    
+    pdf_H0 = multivariate_normal(mean=ut[0],cov=Sigma_ut_zt).pdf(ut_zt) 
+    pdf_H1 = multivariate_normal(mean=ut[1],cov=Sigma_ut_zt).pdf(ut_zt)
+    Gd.plot_2_Gaussian(ut[0],ut[1],round(Sigma_ut_zt,2),round(Sigma_ut_zt,2))
+    plt.plot(ut_zt,pdf_H1,color='kv', linewidth=10)
+    plt.plot(ut_zt,pdf_H0,color='kv', linewidth=10)
+
+    plt.title('decision:% d'% u_D)
+
+    
