33067aab606e5b219d6fc748e8150a8c15a45839
Zhaoliang <zhz03@g.ucla.edu>
2020-09-28 20:11:41 -0500

    add error comparison between estimate and ground truth to the function

--- Estimator/KF_estimator.py
+++ Estimator/KF_estimator.py
@@ -68,42 +68,6 @@ def KF_estimator(SM,measurements):
 
 def figs_verification(num):
     dx = 1
-    Arange = [0,2]
-    Brange = [1,1]
-    Hrange = [0,2]
-    Qrange = [0,2]    
-    Rrange = [0,2]
-    System_models = SMGen1d.SM_generator_1d(num,Arange,Brange,Hrange,Qrange,Rrange)
-    #As,Hs,Bs,Qs,Rs,
-    T,uts,ts,ut,trials,x0 = SSGen.System_setup_generator()
-    SM_num = len(System_models[0])
-    
-    for i in range(SM_num):
-        A = System_models[0][i]
-        B = System_models[1][i]
-        H = System_models[2][i]
-        Q = System_models[3][i]
-        R = System_models[4][i]
-        SM = [A,B,H,Q,R]
-        u = Gsequ.generate_sequential_ut(uts,ts)
-        y,z = Simu.Simulator(SM,x0,u)
-        Sigma,estimates = KF_estimator(SM,z)
-        
-        ground_truth = cnvdata.convert_array2list_nd(y,dx)
-        measurements = cnvdata.convert_array2list_nd(z,dx) 
-        estimates = cnvdata.convert_array2list_nd(estimates,dx)
-        errors = estimates - measurements
-        #predictions = cnvdata.convert_array2list_nd(predictions,dx)
-        plotfgs.multiKf_plot(measurements,ground_truth,estimates,kfest_flag = True)
-        plt.title('A=' + '%.2f' % A + '; B=' + '%.2f' % B + '; H=' + '%.2f' % H + '; Q=' + '%.2f' % Q + '; R=' + '%.2f' % R)
-        fig_name = './figs/KF_estimator_figs/' + str(i) + '.jpg'
-        plt.savefig(fig_name)
-        plt.close()
-        
-if __name__ == '__main__':
-    #figs_verification(20)
-    num = 1
-    dx = 1
     Arange = [1,1]
     Brange = [1,1]
     Hrange = [0,2]
@@ -143,6 +107,10 @@ if __name__ == '__main__':
         fig_name1 = './figs/KF_estimator_figs/' + str(i) + '_error.jpg'
         plt.savefig(fig_name1)
         plt.close()
+        
+if __name__ == '__main__':
+    figs_verification(20)
+
     """
     dx = 2
     dz = 2
