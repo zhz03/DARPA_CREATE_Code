6c723caca667acca21a9a811081b7e6f51f568f0
Zhaoliang <zhz03@g.ucla.edu>
2020-10-14 15:42:45 -0500

    debug

--- binary_HT_nd/Error_prob_cal_verification.py
+++ binary_HT_nd/Error_prob_cal_verification.py
@@ -27,6 +27,15 @@ def Epr_verification_1d():
     return Pr_D,Pr_FA,Pr_M,Pr_CR
 
 if __name__ == "__main__":
+    mean0 = np.array([0,0])
+    mean1 = np.array([1,1])
+    Sigma0 = np.array([[2,-1],[-1,2]]).reshape(2, 2)
+    Sigma1 = np.array([[3,0.1],[0.1,3]]).reshape(2, 2)
+    points = smpl_bGDnd.sampling_binGDnd(mean0,mean1,Sigma0,Sigma1,1000)
+
+    #p0 = multivariate_normal(mean=mean0,cov=Sigma0).pdf(points[0])
+    p1 = multivariate_normal(mean=mean1,cov=Sigma1).pdf(points[0])  
+    
     filepath = './data_storage/verification_2d/'
     Sample_points,M0,M1,S0,S1 = smpl_bGDnd_plt.load_plt_data(filepath) 
     points1 = Sample_points[1]
@@ -36,14 +45,7 @@ if __name__ == "__main__":
     s1 = S1[1]
     p0 = multivariate_normal(mean=m0,cov=s0).pdf(points[1])
     
-    mean0 = np.array([0,0])
-    mean1 = np.array([1,1])
-    Sigma0 = np.array([[2,-1],[-1,2]]).reshape(2, 2)
-    Sigma1 = np.array([[3,0.1],[0.1,3]]).reshape(2, 2)
-    points = smpl_bGDnd.sampling_binGDnd(mean0,mean1,Sigma0,Sigma1,1000)
-
-    #p0 = multivariate_normal(mean=mean0,cov=Sigma0).pdf(points[0])
-    p1 = multivariate_normal(mean=mean1,cov=Sigma1).pdf(points[0])    
+  
     #Pr_D,Pr_FA,Pr_M,Pr_CR,Points_D,Points_FA,Points_M,Points_CR = EprC.Error_prob_cal(m0,m1,s0,s1,points,True)
     #plt.plot()
     
