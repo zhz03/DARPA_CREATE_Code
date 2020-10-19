558fc8e22eafc5e0f70cea7bcd192ef316d43041
Zhaoliang <zhz03@g.ucla.edu>
2020-10-19 00:00:36 -0500

    modify binary HT nd verification

--- binary_HT_nd/bin_HT_nd_verification.py
+++ binary_HT_nd/bin_HT_nd_verification.py
@@ -9,7 +9,7 @@ import random
 import numpy as np
 import binary_HT_nd.bin_HT_nd as bHT
 
-def swap(a1,a2):
+def swap_2d(a1,a2):
     if abs(a1) ==  abs(a2):
         a2 = a2 - 0.01
     if abs(a1) >  abs(a2):
@@ -17,7 +17,15 @@ def swap(a1,a2):
     else:        
         return abs(a2),a1
 
+#def generate_cov():
+    
+    
 def generate_param(nd,range1):
+    """
+    mean needs to be generated from a range
+    covariance matrix Sigma needs to be symmatrical
+    
+    """
     if nd == 1:
         m0 = round(random.uniform(range1[0],range1[1]))
         m1 = round(random.uniform(range1[0],range1[1]))
@@ -38,14 +46,17 @@ def generate_param(nd,range1):
         
         s01 = round(random.uniform(range1[0],range1[1]))
         s02 = round(random.uniform(range1[0],range1[1]))
-        s01,s02 = swap(s01,s02)
+        s01,s02 = swap_2d(s01,s02)
         s11 = round(random.uniform(range1[0],range1[1]))
         s12 = round(random.uniform(range1[0],range1[1]))
-        s11,s12 = swap(s11,s12)    
+        s11,s12 = swap_2d(s11,s12)    
         
         Sigma0 = np.array([[s01,s02],[s02,s01]]).reshape(2, 2)
         Sigma1 = np.array([[s11,s12],[s12,s11]]).reshape(2, 2)
+    
+   # elif nd == 3:
         
+     
     return mean0, mean1, Sigma0, Sigma1 
     
 if __name__ == "__main__":
@@ -54,7 +65,7 @@ if __name__ == "__main__":
     mean1 = np.array([1])
     Sigma0 = np.array([1]).reshape(1, 1)
     Sigma1 = np.array([1]).reshape(1, 1)
-    range1 = [-5,5]    
+       
     """
     """
     mean0 = np.array([0,0])
@@ -67,6 +78,7 @@ if __name__ == "__main__":
     Sigma0 = np.array([[2,-1,0],[0,2,0],[0,-1,2]]).reshape(3, 3)
     Sigma1 = np.array([[3,0.1,0],[0,3,0],[0,0.1,3]]).reshape(3, 3)
     """
+    range1 = [-5,5] 
     mean0, mean1, Sigma0, Sigma1 = generate_param(2,range1)
     Num_sam = 1000
     Pr_D,Pr_FA,Pr_M,Pr_CR = bHT.bin_HT_nd(mean0,mean1,Sigma0,Sigma1,Num_sam)
