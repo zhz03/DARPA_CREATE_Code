ef31ceff8983498ed48546533b7354c1112c7bef
Zhaoliang <zhz03@g.ucla.edu>
2020-10-20 22:30:06 -0500

    test 3d data and compare the results with the old version code

--- binary_HT_nd/Error_prob_cal.py
+++ binary_HT_nd/Error_prob_cal.py
@@ -130,6 +130,10 @@ if __name__ == "__main__":
     Sigma0 = np.array([[2,-1],[-1,2]]).reshape(2, 2)
     Sigma1 = np.array([[3,0.1],[0.1,3]]).reshape(2, 2)
     """
+    mean0 = np.array([0,0,0])
+    mean1 = np.array([1,1,1])
+    Sigma0 = np.array([[2,-1,0],[0,2,0],[0,-1,2]]).reshape(3, 3)
+    Sigma1 = np.array([[3,0.1,0],[0,3,0],[0,0.1,3]]).reshape(3, 3)
     points = smpl_bGDnd.sampling_binGDnd(mean0,mean1,Sigma0,Sigma1,1000)
     
     #Pr_D,Pr_FA,Pr_M,Pr_CR = Error_prob_cal(mean0,mean1,Sigma0,Sigma1,points)
