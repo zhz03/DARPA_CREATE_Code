5fdf8de84a43e28d10eb300ea942c3cecf2cffdb
Zhaoliang <zhz03@g.ucla.edu>
2021-01-07 04:28:34 -0800

    finish the SM generator nd

--- E2Etest/SM_generator_nd.py
+++ E2Etest/SM_generator_nd.py
@@ -7,17 +7,36 @@ Created on Thu Jan  7 04:01:53 2021
 import numpy as np
 from multi_HT_nd.input_generator_nd import generate_cov_new as GenCov
 
-def SM_generator_nd(dx,dz,du,Arange,Brange,Hrange,Qrange,Rrange):
+def SM_generator_nd_single(dx,dz,du,Arange,Brange,Hrange,Qrange,Rrange):
     
     A = np.random.uniform(Arange[0], Arange[1], (dx,dx))
     B = np.random.uniform(Brange[0], Brange[1], (dx,du))
     H = np.random.uniform(Hrange[0], Hrange[1], (dz,dx))
-    
     Q = GenCov(Qrange,dx)[0]
     R = GenCov(Rrange,dz)[0]
     #Q = np.random.uniform(Qrange[0], Qrange[1], (dx,dx))
     #R = np.random.uniform(Rrange[0], Rrange[1], (dz,dz))
     SM = [A,B,H,Q,R]
+    return SM
+
+def SM_generator_nd(dx,dz,du,Arange,Brange,Hrange,Qrange,Rrange,num):
+    As = []
+    Hs = []
+    Bs = []
+    Qs = []
+    Rs = []    
+    for i in range(num):
+        A = np.random.uniform(Arange[0], Arange[1], (dx,dx))
+        B = np.random.uniform(Brange[0], Brange[1], (dx,du))
+        H = np.random.uniform(Hrange[0], Hrange[1], (dz,dx))
+        Q = GenCov(Qrange,dx)[0]
+        R = GenCov(Rrange,dz)[0]
+        As.append(A)
+        Bs.append(B)
+        Hs.append(H)
+        Rs.append(R)
+        Qs.append(Q)
+    SM = [As,Bs,Hs,Qs,Rs]
     return SM
 
 if __name__ == "__main__":
@@ -29,4 +48,5 @@ if __name__ == "__main__":
     Hrange = [0,4]
     Qrange = [0,4]    
     Rrange = [0,4]
-    sm = SM_generator_nd(dx,dz,du,Arange,Brange,Hrange,Qrange,Rrange)
+    num = 100
+    sm = SM_generator_nd(dx,dz,du,Arange,Brange,Hrange,Qrange,Rrange,num)
