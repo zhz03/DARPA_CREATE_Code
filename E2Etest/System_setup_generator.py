c7c77f34d865b7d5e462f05de80f07a143e66b25
Zhaoliang <zhz03@g.ucla.edu>
2021-01-07 04:50:32 -0800

    add SS generator nd

--- E2Etest/System_setup_generator.py
+++ E2Etest/System_setup_generator.py
@@ -5,6 +5,7 @@ Created on Wed Sep  9 14:09:33 2020
 @author: Zhaoliang
 """
 import numpy as np
+from multi_HT_nd.input_generator_nd import generate_mean as GenMean
 
 def System_setup_generator():
     
@@ -17,5 +18,19 @@ def System_setup_generator():
     x0 = np.array([[0]]).reshape(dx, 1)
     return T,uts,ts,ut,trials,x0
 
+def System_setup_generator_nd(nHy,nd,MRange):
+    uts = GenMean(nHy,nd,MRange)
+    ts = [100,1]
+    T = ts[0] + ts[1]
+    ut = uts
+    trials = 1000   
+    dx = nd
+    x0 = np.random.uniform(0, 0, (dx,1))
+    return T,uts,ts,ut,trials,x0
+    
 if __name__ == '__main__':
-    T,uts,ts,ut,trials,x0 = System_setup_generator()
+    #T,uts,ts,ut,trials,x0 = System_setup_generator()
+    MRange = [0,3]
+    nHy = 3
+    nd = 2
+    T,uts,ts,ut,trials,x0 = System_setup_generator_nd(nHy,nd,MRange)
