6c4dd7390d1a7f5291c6120ffc3e79daf62efd43
Zhaoliang <zhz03@g.ucla.edu>
2021-01-07 04:19:18 -0800

    debug SM generator nd

--- E2Etest/SM_generator_nd.py
+++ E2Etest/SM_generator_nd.py
@@ -5,14 +5,18 @@ Created on Thu Jan  7 04:01:53 2021
 @author: Zhaoliang
 """
 import numpy as np
+from multi_HT_nd.input_generator_nd import generate_cov_new as GenCov
 
 def SM_generator_nd(dx,dz,du,Arange,Brange,Hrange,Qrange,Rrange):
     
     A = np.random.uniform(Arange[0], Arange[1], (dx,dx))
     B = np.random.uniform(Brange[0], Brange[1], (dx,du))
     H = np.random.uniform(Hrange[0], Hrange[1], (dz,dx))
-    Q = np.random.uniform(Qrange[0], Qrange[1], (dx,dx))
-    R = np.random.uniform(Rrange[0], Rrange[1], (dz,dz))
+    
+    Q = GenCov(Qrange,dx)[0]
+    R = GenCov(Rrange,dz)[0]
+    #Q = np.random.uniform(Qrange[0], Qrange[1], (dx,dx))
+    #R = np.random.uniform(Rrange[0], Rrange[1], (dz,dz))
     SM = [A,B,H,Q,R]
     return SM
 
