Update: fix Decision_making into nd, but exist numerical wired result

E2Etest/System_setup_generator.py
@@ -18,8 +18,8 @@ def System_setup_generator():
     x0 = np.array([[0]]).reshape(dx, 1)
     return T,uts,ts,ut,trials,x0
 
-def System_setup_generator_nd(nHy,nd,MRange):
-    uts = GenMean(nHy,nd,MRange)
+def System_setup_generator_nd(nHy,nd,nu,MRange):
+    uts = GenMean(nHy,nu,MRange)
     ts = []
     for _ in range(nHy):
         ts.append(int(np.random.randint(20,50,size=1)))
@@ -27,8 +27,9 @@ def System_setup_generator_nd(nHy,nd,MRange):
     #T = ts[0] + ts[1]
     T = sum(ts)
     ut = uts
+    print("ut is " , ut)
 
-    trials = 1000   
+    trials = 100   
     dx = nd
     x0 = np.random.uniform(0, 0, (dx,1))
     return T,uts,ts,ut,trials,x0

Estimator/Bayesian_analysis.py
@@ -14,6 +14,7 @@ def Bayesian_analysis(SM,ut,Sigma,estimates,measurements):
     Q = SM[3]
     R = SM[4]
     ut_zt = measurements[-1] - np.dot(H,np.dot(A,estimates[-2]))
+    print("bayesian ut|zt",ut_zt)
     Sigma_t = Sigma[-1]
     S = np.dot(np.dot(A,Sigma_t),A.T)+Q
     Sigma_ut_zt01 = np.dot(np.dot(H,S),H.T)+R
@@ -21,9 +22,15 @@ def Bayesian_analysis(SM,ut,Sigma,estimates,measurements):
     mean_ut_zt = []
     Sigma_ut_zt = []
     
-    for i in range(len(ut))
+    for i in range(len(ut)):
         mean_ut_zt.append(np.dot(H,B) * ut[i])
-        Sigma_ut_zt.append(Sigma_ut_zt01)
+        if (type(ut[i]) == int):
+            Sigma_ut_zt.append(Sigma_ut_zt01)
+        else:
+            #sigma = np.full((H.shape[0], ut[i].shape[-1]), Sigma_ut_zt01)
+            sigma = Sigma_ut_zt01  #Note: dimention is not correct!
+            Sigma_ut_zt.append(sigma)
+        
     #mean_ut_zt = [np.dot(H,B)*ut[0],np.dot(H,B)*ut[1]]
     #Sigma_ut_zt = [Sigma_ut_zt01,Sigma_ut_zt01]
     return ut_zt,mean_ut_zt,Sigma_ut_zt

Estimator/Decision_making.py
@@ -9,18 +9,44 @@ from scipy.stats import multivariate_normal
 
 
 def Decision_making(ut,ut_zt,mean_ut_zt,Sigma_ut_zt):
-    Sigma_ut_zt0 = Sigma_ut_zt[0]
-    Sigma_ut_zt1 = Sigma_ut_zt[1]
-    mean0 = mean_ut_zt[0]
-    mean1 = mean_ut_zt[1]
-    ut0 = ut[0]
-    ut1 = ut[1]
-    print("ut is ", ut)
-    pdf_H0 = multivariate_normal(mean=mean0,cov=Sigma_ut_zt0).pdf(ut_zt) 
-    pdf_H1 = multivariate_normal(mean=mean1,cov=Sigma_ut_zt1).pdf(ut_zt) 
-    if pdf_H0 >= pdf_H1:
-        decide_u = ut0
-    else: decide_u = ut1
+    
+    if ( (len(mean_ut_zt) == 2) & (mean_ut_zt[0].shape[0] == 1)):
+        Sigma_ut_zt0 = Sigma_ut_zt[0]
+        Sigma_ut_zt1 = Sigma_ut_zt[1]
+        mean0 = mean_ut_zt[0]
+        mean1 = mean_ut_zt[1]
+        ut0 = ut[0]
+        ut1 = ut[1]
+        #print("mean is {}, cov is {}. pdf is {} ".format(mean0, Sigma_ut_zt0,ut_zt  ))
+        #print("1d: pdf ut|zt is ", ut_zt)
+        pdf_H0 = multivariate_normal(mean=mean0,cov=Sigma_ut_zt0).pdf(ut_zt) 
+        pdf_H1 = multivariate_normal(mean=mean1,cov=Sigma_ut_zt0).pdf(ut_zt) 
+        if pdf_H0 >= pdf_H1:
+            decide_u = ut0
+        else: decide_u = ut1
+        print("1d: decide u is", decide_u)
+    else:
+        pdf_H_list = []
+        for i in range(len(mean_ut_zt)):
+            mean_cur = mean_ut_zt[i] # 1*n 
+            Sigma_ut_zt_cur = Sigma_ut_zt[i] # scalar
+            
+            #dim_mean = mean_cur.shape[-1]
+            #sigma = np.full((dim_mean,dim_mean), Sigma_ut_zt_cur)
+            print("mean is {}, cov is {}. pdf is {}".format(mean_cur.flatten(),int(Sigma_ut_zt_cur),  ut_zt.flatten()))
+            #print("nd: pdf ut|zt is ", ut_zt)
+            pdf_H_cur = multivariate_normal(mean=mean_cur.flatten() ,cov=int(Sigma_ut_zt_cur)).pdf(ut_zt.flatten())
+            pdf_H_list.append(pdf_H_cur)
+  #          print("mean.dim is {}, cov.dim is {}, ut_dim is {}, pdf.dim is {}, pdf_H_cur".
+  #                format(mean_cur.flatten().shape, Sigma_ut_zt_cur.shape, 
+  #                       ut_zt.T.shape, pdf_H_cur.shape, pdf_H_cur)
+  #                )          
+            
+        max_pdf_H = pdf_H_list.index(max(pdf_H_list))
+            
+        decide_u  = ut[max_pdf_H]
+        print("nd: decide u is", decide_u)
+        
     return decide_u
 
 if __name__ == '__main__':

Simulations/Simulator.py
@@ -62,7 +62,6 @@ class Simulator_1t(object):
         # process noise
         #self.process_noise = self.convert_var2noise(self.process_var)
         self.process_noise = self.multi_noise(self.process_var)
-
         ut = np.dot(self.B,self.ut) +  self.process_noise
         self.x =np.dot(self.A,self.x)  + np.dot(ut, dt)
     def sensing(self):
@@ -129,9 +128,13 @@ def Simulator(SM,x0,ut_sq):
             xzs_new = [sim_1n.transfer_and_sense()]
             yt,zt = seperate_x_z(xzs_new)
 
-            #if yt[0][0] >= 1000000 or yt[0][0] <= -1000000 :
-            #    yt[0][0] = yt[0][0]/1000000
-            #    zt[0][0] = zt[0][0]/1000000
+        #    if yt[0][0] >= 1000000 or yt[0][0] <= -1000000 :
+         #       yt[0][0] = yt[0][0]/1000000
+        #        zt[0][0] = zt[0][0]/1000000
+                
+            if np.mean(yt[0]) >=1000000 or np.mean(yt[0]) <= -1000000:
+                yt[0] /= 1000000
+                zt[0] /= 1000000
 
             y.append(yt[0])
             z.append(zt[0])